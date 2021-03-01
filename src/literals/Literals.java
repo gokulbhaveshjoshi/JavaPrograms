d72d22b7e37bb04ccf7be58697807cc3936c1215
Bhavesh Joshi <34483890+gokulbhaveshjoshi@users.noreply.github.com>
2021-03-01 22:24:00 +0530

    Day 2
    Literals

---
+++ src/literals/Literals.java
@@ -0,0 +1,23 @@
+package literals;
+
+public class Literals {
+
+	public static void main(String[] args) {
+		int i = 1;
+		boolean flag = false;    // Boolean
+		String str = "name";    // String
+		int oct = 34;           // number
+		int hexa = 0x2f;        //Hexa number
+		int bin = 0b1010;       // Binary number
+		double myDouble = 3.4;  // double
+		float myFloat = 3.4f;   //float
+		double d = 3.44e2;      // 3.44*10*2
+		char letter = 'a';
+		
+		System.out.println("Interger: "+i+"\n Boolean: "+flag+ "\n String: "+str+
+				"\nBinary: "+bin+"\n Hexa:" +hexa+"\n Double: "+d
+				);
+
+	}
+
+}
