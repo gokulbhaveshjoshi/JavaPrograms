caf88aa494a4f34e2bf50ddb632b8c94f441d8ca
Bhavesh Joshi <34483890+gokulbhaveshjoshi@users.noreply.github.com>
2021-03-05 22:23:49 +0530

    Arithemetic Operation
    Day 4

--- src/literals/Literals.java
+++ src/literals/Literals.java
@@ -7,7 +7,7 @@ public class Literals {
 		boolean flag = false;    // Boolean
 		String str = "name";    // String
 		int oct = 34;           // number
-		int hexa = 0x2f;        //Hexa number
+		int hexa = 0x2f;        //Hexanumber
 		int bin = 0b1010;       // Binary number
 		double myDouble = 3.4;  // double
 		float myFloat = 3.4f;   //float
---
+++ src/operators/ArithmeticOperator.java
@@ -0,0 +1,21 @@
+package operators;
+
+/*
+ *
+ * Arithemetic Operation are used to perform arithemetic operation on variable and data.
+ * eg:- a+b, a-b, a*b, a/b, a%b
+ */
+
+public class ArithmeticOperator {
+
+	public static void main(String[] args) {
+		int a = 20, b= 10;
+		System.out.println("Addition of a and b: "+ (a+b));
+		System.out.println("Subtraction of a and b: "+ (a-b));
+		System.out.println("Multiplication of a and b: "+ (a*b));
+		System.out.println("Division of a and b: "+ (a/b));
+		System.out.println("Modules of a and b: "+ (a%b));
+
+	}
+
+}
