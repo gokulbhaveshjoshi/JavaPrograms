53af25ced8678d2fc3c85bf626a5d12e9b10decb
Bhavesh Joshi <34483890+gokulbhaveshjoshi@users.noreply.github.com>
2021-04-18 11:48:10 +0530

    maintain flow

--- src/programs/Largest3.java
+++ src/programs/Largest3.java
@@ -4,29 +4,32 @@ public class Largest3 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int a[] = {5,3,8,10,2};
-		int first = 0, second =  0, third = 0;
+		int a[] = { 5, 3, 8, 10, 2 ,15,71,25,36,108,3,96,98};
+		int first = 0, second = 0, third = 0;
 		for (int i : a) {
-			if(first<i) {
+			
+			// check for first number
+			if (first < i) {
 				int temp = first;
 				first = i;
 				third = second;
 				second = temp;
 			}
 			
-			if(first > i && second < i) {
+			// check for second number
+			if (first > i && second < i) {
 				int temp = second;
 				second = i;
-				third = second;
+				third = temp;
 			}
 			
-			if((first > i && second < i) && third < i) {
+			// check for third number
+			if ((first > i && second < i) && third < i) {
 				third = i;
 			}
-			
 
 		}
-		
-		System.out.println("first: "+ first+" second : "+ second+" third "+third);
+
+		System.out.println("first: " + first + " second : " + second + " third " + third);
 	}
 }
